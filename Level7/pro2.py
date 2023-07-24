21e3241d7b78a371833b6dacd43e2441b093d1af
Joosewon <joosewon@naver.com>
2023-07-24 18:10:00 +0900

    7/24

---
+++ Level7/pro2.py
@@ -0,0 +1,12 @@
+max_num = 0
+
+for i in range(9):
+    row = list(map(int, input().split()))
+    if max(row) > max_num:
+        max_num = max(row) #최댓값
+        x = i + 1 #행
+        y = row.index(max_num) + 1 #열
+print(max_num)
+print(x,y)
+
+
